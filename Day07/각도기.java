861b15a3986770df06c22ccc728b3a7ad2ff2def
Dev.JHJ <yg9316@naver.com>
2023-02-25 23:13:32 +0900

    Create 각도기.java

---
+++ Day07/각도기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int angle) {
+        if(angle == 180)
+            return 4;
+        else if(angle > 90)
+            return 3;
+        else if(angle == 90)
+            return 2;
+        else
+            return 1;
+    }
+}
