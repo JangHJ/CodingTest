8cb857318a9994d8532027f2ab05ba10fa5e2cdb
JangHJ <yg9316@swu.ac.kr>
2023-04-15 23:49:14 +0900

    Day22 코딩테스트 입문 문제풀이 2 수정완료

--- Day22/평행.java
+++ Day22/평행.java
@@ -1,24 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.lang.Math;
-
 class Solution {
-	public int solution(int[][] dots) {
-		int answer = 0;
-		ArrayList<Double> slopes = new ArrayList<>();
+    public double slope(int[] dot1, int[] dot2) {
+        //기울기 공식 (y2-y1)==(x2-x1)
+        return (double) (dot2[1] - dot1[1]) / (dot2[0] - dot1[0]);
+    }
 
-		for (int i = 0; i < dots.length-1; i++) { //0~1
-			for (int j = i+1; j < dots.length; j++) { //1~2
+    public int solution(int[][] dots) {
+        int answer = 0;
 
-				double y = dots[i][1] - dots[j][1]; //y값의 증가량
-				double x = dots[i][0] - dots[j][0]; //x값의 증가량
+		//[a-b, c-d],[a-c, b-d],[a-d, b-c] 4개의 점들로 임의의 두 쌍을 만들었을 때
+        if (slope(dots[0], dots[1]) == slope(dots[2], dots[3])) {
+            return 1;
+        }
 
-				double slop = Math.abs(y/x); // 기울기 공식
+        if (slope(dots[0], dots[2]) == slope(dots[1], dots[3])) {
+            return 1;
+        }
 
-				if (!slopes.contains(slop)) slopes.add(slop);
-                else return 1;
-			}
-		}
-		return answer;
-	}
+        if (slope(dots[0], dots[3]) == slope(dots[1], dots[2])) {
+            return 1;
+        }
+
+        return answer;
+    }
 }
