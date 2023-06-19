ba642ee198398c8c686a268a5df3cc247b780231
JangHJ <yg9316@swu.ac.kr>
2023-06-20 00:05:47 +0900

    Day23 코딩테스트 입문 문제풀이 3

---
+++ Day23/옹알이 (1).java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        
+        for(int i=0; i<babbling.length; i++){
+            babbling[i] = babbling[i].replace("aya", "1");
+            babbling[i] = babbling[i].replace("ye", "1");
+            babbling[i] = babbling[i].replace("woo", "1");
+            babbling[i] = babbling[i].replace("ma", "1");
+            babbling[i] = babbling[i].replace("1", "");
+            
+            if(babbling[i].equals("")){
+                answer += 1;
+            }
+        }
+        return answer;
+    }
+}
