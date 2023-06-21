ba1217763931aa9d624e4fe8af8a583603e0d60f
JangHJ <yg9316@swu.ac.kr>
2023-06-21 23:42:28 +0900

    Day22 코딩테스트 입문 문제풀이 3

---
+++ Day22/겹치는 선분의 길이.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[][] lines) {
+        int answer = 0;
+        int[] arr = new int[200];
+        
+        for(int i=0 ;i<lines.length; i++){
+            for(int j = lines[i][0] + 100; j < lines[i][1]+100; j++){
+                arr[j]++;
+            }
+        }
+        
+        for(int a : arr){
+            if(a > 1)
+                answer++;
+        }
+        
+        return answer;
+    }
+}
